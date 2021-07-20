21761c2f8520b562264e0919191f84bb28fdce26
Agust Rinaldo Kurniawan <agust.kurniawan@Agust-Rinaldo-Kurniawan.local>
2021-07-20 17:34:10 +0700

    skill

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function showSkill(id) {
     var show = document.getElementById(id).style.display
     if(show){
         document.getElementById(id).style.display = ""
+        document.getElementById(`${id}-icon`).src = "assets/icons/akar-icons_chevron-down-1.png"
     } else{
         document.getElementById(id).style.display = "flex"
+        document.getElementById(`${id}-icon`).src = "assets/icons/akar-icons_chevron-down.png"
     }
 }
